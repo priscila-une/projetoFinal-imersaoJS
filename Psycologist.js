f90e396d215a8d2b2bdd54656a4797b6fb5dbecb
Priscila Une <prixlin@gmail.com>
2022-12-09 19:58:44 -0300

    update psycologist class

--- Psycologist.js
+++ Psycologist.js
@@ -3,12 +3,20 @@ const Person = require('./Person');
 class Psycologist extends Person {
     
     schedules = [];
+    static psycologistList = [];
 
     constructor(name, email, schedules){
         super(name, email)
         this.schedules = schedules;
+        this.constructor.psycologistList.push({
+            name: this.name,
+            schedules: this.schedules
+        })
     }
 
+    getName(){
+        return this.name;
+    }
 }
 
 
