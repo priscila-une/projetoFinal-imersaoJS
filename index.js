3cf84b5a71d83abc27780e82e9d0da5b5db863e4
Priscila Une <prixlin@gmail.com>
2022-12-09 19:59:15 -0300

    update index

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-const Person = require('./Person');
+
 const Mother = require('./Mothers');
 const Psycologist = require('./Psycologist');
-const Appointment = require('./Appointment');
+
+
 
 const mother1 = new Mother("Evelin", "evelin@email.com");
-const psycologist1 = new Psycologist("Janaina", "janaina@email.com", ["9:00", "10:00"])
-const appointment1 = new Appointment(mother1, psycologist1, psycologist1.schedules);
+const psycologist1 = new Psycologist("Janaina", "janaina@email.com", ["9:00", "10:00"]);
+const psycologist2 = new Psycologist("Fernanda", "fernanda@email.com", ["13:00", "14:00", "16:00"]);
+const motherError = new Mother("Evelin");
+console.log(motherError)
+console.log(mother1.scheduleTime());
 
-//console.log(person1);
-//console.log(mother1);
-//console.log(psycologist1);
-console.log(appointment1);
+
 
