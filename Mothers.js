4cb4a391ef8e4632c014339736036ca302012987
Priscila Une <prixlin@gmail.com>
2022-12-09 19:58:09 -0300

    update mothers class

--- Mothers.js
+++ Mothers.js
@@ -1,16 +1,28 @@
 const Person = require('./Person');
+const readlineSync = require('readline-sync');
+const { psycologistList } = require('./Psycologist');
 
 class Mother extends Person {
-
+ 
     constructor(name, email){
         super(name, email);
         
     }
-
+    
     scheduleTime(){
-        //agendar horario
 
+        console.log(`\n Welcome ${this.name}, let's start the appointment.`)
+        console.table(psycologistList);
+        
+        const formattedListPsycologistName = psycologistList.map(psy => `${psy.name} `)
+        const index = readlineSync.keyInSelect(formattedListPsycologistName, "Choose one psychologist: ", {cancel: false})
+        console.log("You choose psycologist " + formattedListPsycologistName[index] )
+        
+        const formattedListSchedules = psycologistList.map(psy => `${psy.schedules[index]}`)
+        const time = readlineSync.keyInSelect(formattedListSchedules, "Choose time do you want to schedule: ", {cancel: false})
+        console.log(` ${formattedListSchedules[time]}\n scheduled time successfuly`);
     }
+    
 }
 
 module.exports = Mother;
