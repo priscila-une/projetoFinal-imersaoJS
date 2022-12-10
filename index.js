857377b9c8788d6b693da1bcdaaeacc1346de532
Priscila Une <prixlin@gmail.com>
2022-12-09 21:29:50 -0300

    fix index

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const Psycologist = require('./Psycologist');
 const mother1 = new Mother("Evelin", "evelin@email.com");
 const psycologist1 = new Psycologist("Janaina", "janaina@email.com", ["9:00", "10:00"]);
 const psycologist2 = new Psycologist("Fernanda", "fernanda@email.com", ["13:00", "14:00", "16:00"]);
-const motherError = new Mother("Evelin");
-console.log(motherError)
+
+
 console.log(mother1.scheduleTime());
 
 
